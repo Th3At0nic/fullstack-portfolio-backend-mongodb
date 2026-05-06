a76491c579795d7309d9b21428387a30c1b1952b
Md Rahatul Islam <islammdrahatul@gmail.com>
2026-05-06 18:02:31 +0600

    refrac: removed warnings via lint:fix command

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const notFound = (req: Request, res: Response, next: NextFunction) => {
   res.status(404).json({
     success: false,
