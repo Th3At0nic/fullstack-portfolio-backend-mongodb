dc72123786f2f3dcc136a37bfadaeafebf241b4f
Md Rahatul Islam <islammdrahatul@gmail.com>
2026-07-24 16:39:45 +0600

    refrac: changed the function name from PersonalProject to Project in controller file in project module

--- src/app/module/Project/project.controller.ts
+++ src/app/module/Project/project.controller.ts
@@ -1,27 +1,26 @@
- 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { StatusCodes } from 'http-status-codes';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
-// import { ProjectService } from './project.service';
+import { ProjectService } from './project.service';
 
-// const createProject = catchAsync(async (req, res, next) => {
+const createProject = catchAsync(async (req, res, next) => {
   const file = req?.file as Express.Multer.File;
 
-  // const result = await ProjectService.createProjectIntoDB(file, req.body);
+  const result = await ProjectService.createProjectIntoDB(file, req.body);
 
   const message = 'Project Added Successfully';
 
   sendResponse(res, StatusCodes.CREATED, true, message, result);
 });
 
-// const getAllProjects = catchAsync(async (req, res, next) => {
-//   const result = await ProjectService.getAllProjectsFromDB();
+const getAllProjects = catchAsync(async (req, res, next) => {
+  const result = await ProjectService.getAllProjectsFromDB();
   const message = 'All Projects Retrieved Successfully';
   sendResponse(res, StatusCodes.OK, true, message, result);
 });
 
-// export const ProjectController = {
-//   createProject,
-//   getAllProjects,
+export const ProjectController = {
+  createProject,
+  getAllProjects,
 };
